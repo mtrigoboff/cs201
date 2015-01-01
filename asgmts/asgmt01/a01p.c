736ee06cff1c55691ac452a9e66af2d3b27d1268
mtrigoboff <devnull@localhost>
2015-01-01 15:09:09 -0800

    changed int to unsigned int where needed

--- asgmts/asgmt01/a01p.c
+++ asgmts/asgmt01/a01p.c
@@ -18,11 +18,11 @@ void bigOrSmallEndian()
 // get next int (entered in hex) using scanf()
 // returns 1 (success) or 0 (failure)
 // if call succeeded, return int value via i pointer
-int getNextHexInt(int *i)
+int getNextHexInt(unsigned int *i)
 {
-    // replace this code with the call to scanf()
-    *i = 0;
-    return 1;
+	// replace this code with the call to scanf()
+	*i = 0;
+	return 1;
 }
 
 // print requested data for the given number
@@ -33,25 +33,25 @@ void printNumberData(int i)
 // do not change this function in any way
 int main(int argc, char **argv)
 {
-    int     i;                              // number currently being analyzed
-    int     nValues;                        // number of values successfully parsed by scanf
+	unsigned int	i;					// number currently being analyzed
+	int				nValues;			// number of values successfully parsed by scanf
 
-    printf("CS201 - A01p - %s\n\n", studentName);
-    bigOrSmallEndian();
-    for (;;) {
-        if (argc == 1)                      // allow grading script to control ...
-            printf("> ");                   // ... whether prompt character is printed
-        nValues = getNextHexInt(&i);
-        printf("0x%08X\n", i);
-        if (! nValues) {                    // encountered bad input
-            printf("bad input\n");
-            while (getchar() != '\n') ;     // flush bad line from input buffer
-            continue;
-            }
-        printNumberData(i);
-        if (i == 0)
-            break;
-        }
-    printf("\n");
-    return 0;
+	printf("CS201 - A01p - %s\n\n", studentName);
+	bigOrSmallEndian();
+	for (;;) {
+	if (argc == 1)						// allow grading script to control ...
+		printf("> ");					// ... whether prompt character is printed
+	nValues = getNextHexInt(&i);
+	printf("0x%08X\n", i);
+	if (! nValues) {					// encountered bad input
+		printf("bad input\n");
+		while (getchar() != '\n') ;		// flush bad line from input buffer
+		continue;
+		}
+	printNumberData(i);
+	if (i == 0)
+		break;
+	}
+	printf("\n");
+	return 0;
 }
